2f2e7bb3b27dc5f129e689b2df951290b683a35a
Sanjeev Rai <sunzeevraee@gmail.com>
2025-06-03 15:42:17 +0545

    Just some correction for 1.1

--- part-1/courseInfo/course-info/src/App.jsx
+++ part-1/courseInfo/course-info/src/App.jsx
@@ -1,4 +1,35 @@
 import "./App.css";
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  );
+};
 
 const App = () => {
   const course = "Half Stack application development";
@@ -9,43 +40,22 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
-  const Header = (props) => {
-    return (
-      <>
-        <h1>{props.course}</h1>
-      </>
-    );
-  };
-
-  const Content = () => {
-    return (
-      <>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-      </>
-    );
-  };
-
-  const Total = () => {
-    return (
-      <>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-      </>
-    );
-  };
-
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
