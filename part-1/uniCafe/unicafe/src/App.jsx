54cac3488efcb7ff09ec9622bbf3dad19ae48489
Sanjeev Rai <sunzeevraee@gmail.com>
2025-06-04 22:46:06 +0545

    1.7 exercise completed

--- part-1/uniCafe/unicafe/src/App.jsx
+++ part-1/uniCafe/unicafe/src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 
 const App = () => {
+  //This is 1.7
   // save clicks of each button to its own state
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
