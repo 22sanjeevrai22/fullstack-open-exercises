cc6db8b8ac5e561d6bc063141a051f815ede26a7
Sanjeev Rai <sunzeevraee@gmail.com>
2025-06-05 22:47:15 +0545

    2.8 exercise completed

--- part-2/Part-2/src/App.tsx
+++ part-2/Part-2/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 interface Person {
   name: string;
+  number?: string;
 }
 
 const App = () => {
   const [persons, setPersons] = useState<Person[]>([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const checkDuplicate = () => {
     return persons.some(
@@ -13,8 +15,12 @@ const App = () => {
     );
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewNumber(e.target.value);
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -23,9 +29,10 @@ const App = () => {
     if (checkDuplicate()) {
       alert(`${newName} is already added to phonebook`);
     } else {
-      setPersons((prev) => [...prev, { name: newName }]);
+      setPersons((prev) => [...prev, { name: newName, number: newNumber }]);
     }
     setNewName("");
+    setNewNumber("");
   };
 
   console.log("newname", newName);
@@ -34,7 +41,20 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <input value={newName} onChange={handleChange} />
+          Name:{" "}
+          <input
+            placeholder="Input Name"
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          Number:{" "}
+          <input
+            placeholder="Input Number"
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -42,7 +62,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <div key={person.name}>{person?.name}</div>
+        <div key={person.name}>
+          {person?.name}
+          <span>{person?.number}</span>
+        </div>
       ))}
     </div>
   );
