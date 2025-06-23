dc9f7c18f564a38656e9137b8663538b4fa30228
Sanjeev Rai <sunzeevraee@gmail.com>
2025-06-23 12:27:50 +0545

    Exercise 4.3-4.7 Completed

--- part-4/blog/utils/list_helper.js
+++ part-4/blog/utils/list_helper.js
@@ -4,6 +4,57 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favouriteBlog = (blogList) => {
+  return blogList.reduce((acc, currentBlog) => {
+    return currentBlog.likes > acc.likes ? currentBlog : acc;
+  }, blogList[0]);
+};
+
+const mostBlogs = (blogList) => {
+  if (blogList.length === 0) return null;
+  const count = {};
+  blogList.forEach((blog) => {
+    count[blog.author] = (count[blog.author] || 0) + 1;
+  });
+  let maxAuthor = null;
+  let maxBlogs = 0;
+
+  for (const author in count) {
+    if (count[author] > maxBlogs) {
+      maxBlogs = count[author];
+      maxAuthor = author;
+    }
+  }
+  return { author: maxAuthor, blogs: maxBlogs };
+};
+
+const mostLikes = (blogList) => {
+  if (blogList.length === 0) return null;
+  const likeCount = {};
+  blogList.forEach((blog) => {
+    // likeCount[blog.author] = (likeCount[blog.author] || 0) + blog.likes;
+    if (!likeCount[blog.author]) {
+      likeCount[blog.author] = blog.likes;
+    } else {
+      likeCount[blog.author] += blog.likes;
+    }
+  });
+
+  let maxAuthor = null;
+  let maxLikes = 0;
+
+  for (const author in likeCount) {
+    if (likeCount[author] > maxLikes) {
+      maxLikes = likeCount[author];
+      maxAuthor = author;
+    }
+  }
+  return { author: maxAuthor, likes: maxLikes };
+};
+
 module.exports = {
   totalLikes,
+  favouriteBlog,
+  mostBlogs,
+  mostLikes,
 };
