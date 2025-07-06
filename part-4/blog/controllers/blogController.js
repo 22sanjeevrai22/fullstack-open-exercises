5074e75a67294e1cd8e24a83bd5dcaa6a886276a
Sanjeev Rai <sunzeevraee@gmail.com>
2025-07-06 13:20:24 +0545

    Ex.4.21 Completed.
    Enhance blog deletion logic to include user authentication and authorization checks

--- part-4/blog/controllers/blogController.js
+++ part-4/blog/controllers/blogController.js
@@ -92,10 +92,36 @@ blogRouter.put("/:id", async (req, res, next) => {
 
 blogRouter.delete("/:id", async (req, res, next) => {
   try {
-    const id = req.params.id;
-    const deletedBlog = await Blog.findByIdAndDelete(id);
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: "token invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
+      return res.status(400).json({ error: "userId missing or not valid" });
+    }
+
+    const blogId = req.params.id;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(404).json({ error: "Blog not found" });
+    }
+
+    if (blog.user.toString() !== user.id.toString()) {
+      return res
+        .status(403)
+        .json({ error: "Only the creator can delete this blog" });
+    }
+
+    const deletedBlog = await Blog.findByIdAndDelete(blogId);
+
     if (deletedBlog) {
-      res.status(204).end();
+      res.status(200).json({
+        message: `Blog with id ${blogId} deleted.`,
+        data: deletedBlog,
+      });
     } else {
       res.status(404).json({ error: "Blog not found" });
     }
