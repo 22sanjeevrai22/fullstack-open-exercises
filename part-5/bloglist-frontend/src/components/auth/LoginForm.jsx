fdc83265429f78b3d77675c71329ce6bf965c1e0
Sanjeev Rai <sunzeevraee@gmail.com>
2025-07-22 11:29:13 +0545

    Ex 5.6, 5.7 Completed

---
+++ part-5/bloglist-frontend/src/components/auth/LoginForm.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { login } from "../../services/authService";
+
+const LoginForm = ({ handleSetUser, setErrorMessageWrapper }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await login({ username, password });
+      window.localStorage.setItem("blogUserInfo", JSON.stringify(user));
+      handleSetUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessageWrapper("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessageWrapper(null);
+      }, 5000);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            placeholder="Username hmm.."
+            value={username}
+            type="text"
+            name="username"
+            autoComplete="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            placeholder="Password hmm.."
+            value={password}
+            type="password"
+            name="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
