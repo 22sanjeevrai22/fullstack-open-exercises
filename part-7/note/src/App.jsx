0ae62d1b06e4c0b6a03916e1d43e09d637bf1fb2
Sanjeev Rai <sunzeevraee@gmail.com>
2025-11-12 15:53:43 +0545

    Refactor App component to use class-based structure and integrate axios for fetching anecdotes; create App2 component with original functional structure for comparison.

--- part-7/note/src/App.jsx
+++ part-7/note/src/App.jsx
@@ -1,229 +1,32 @@
-import { useState } from "react";
-import {
-  Container,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Paper,
-  TextField,
-  // Button,
-  Alert,
-  Toolbar,
-  IconButton,
-  AppBar,
-} from "@mui/material";
+import React from "react";
+import axios from "axios";
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anecdotes: [],
+      current: 0,
+    };
+  }
 
-import {
-  Link,
-  Navigate,
-  Route,
-  Routes,
-  useMatch,
-  useNavigate,
-} from "react-router-dom";
+  componentDidMount = () => {
+    axios.get("http://localhost:3001/anecdotes").then((response) => {
+      this.setState({ anecdotes: response.data });
+    });
+  };
 
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: Bisque;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid Chocolate;
-  border-radius: 3px;
-`;
-
-const Input = styled.input`
-  margin: 0.25em;
-`;
-
-const Page = styled.div`
-  padding: 1em;
-  background: papayawhip;
-`;
-
-const Navigation = styled.div`
-  background: BurlyWood;
-  padding: 1em;
-`;
-
-const Footer = styled.div`
-  background: Chocolate;
-  padding: 1em;
-  margin-top: 1em;
-`;
-
-const Home = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      Lorem Ipsum has been the industry's standard dummy text ever since the
-      1500s, when an unknown printer took a galley of type and scrambled it to
-      make a type specimen book. It has survived not only five centuries, but
-      also the leap into electronic typesetting, remaining essentially
-      unchanged. It was popularised in the 1960s with the release of Letraset
-      sheets containing Lorem Ipsum passages, and more recently with desktop
-      publishing software like Aldus PageMaker including versions of Lorem
-      Ipsum.
-    </p>
-  </div>
-);
-
-const Note = ({ note }) => {
-  return (
-    <div>
-      <h2>{note.content}</h2>
-      <div>{note.user}</div>
+  render() {
+    if (this.state.anecdotes.length === 0) {
+      return <div>no anecdotes...</div>;
+    }
+    return (
       <div>
-        <strong>{note.important ? "important" : ""}</strong>
+        <h1>anecdote of the day</h1>
+        <div>{this.state.anecdotes[this.state.current].content}</div>
+        <button>next</button>
       </div>
-    </div>
-  );
-};
-
-const Notes = ({ notes }) => (
-  <div>
-    <h2>Notes</h2>
-
-    <TableContainer component={Paper}>
-      <Table>
-        <TableBody>
-          {notes.map((note) => (
-            <TableRow key={note.id}>
-              <TableCell>
-                <Link to={`/notes/${note.id}`}>{note.content}</Link>
-              </TableCell>
-              <TableCell>{note.user}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </div>
-);
-
-const Users = () => (
-  <div>
-    <h2>TKTL notes app</h2>
-    <ul>
-      <li>Matti Luukkainen</li>
-      <li>Juha Tauriainen</li>
-      <li>Arto Hellas</li>
-    </ul>
-  </div>
-);
-
-const Login = (props) => {
-  const navigate = useNavigate();
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    props.onLogin("mluukkai");
-    navigate("/");
-  };
-
-  return (
-    <div>
-      <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username:
-          <Input />
-        </div>
-        <div>
-          password:
-          <Input type="password" />
-        </div>
-        <Button type="submit" primary="">
-          login
-        </Button>
-      </form>
-    </div>
-  );
-};
-
-const App = () => {
-  const [notes, setNotes] = useState([
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true,
-      user: "Matti Luukkainen",
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false,
-      user: "Matti Luukkainen",
-    },
-    {
-      id: 3,
-      content: "Most important methods of HTTP-protocol are GET and POST",
-      important: true,
-      user: "Arto Hellas",
-    },
-  ]);
-  const [message, setMessage] = useState(null);
-
-  const match = useMatch("/notes/:id");
-  const note = match
-    ? notes.find((note) => note.id === Number(match.params.id))
-    : null;
-
-  const [user, setUser] = useState(null);
-
-  const login = (user) => {
-    setUser(user);
-    setMessage(`welcome ${user}`);
-    setTimeout(() => {
-      setMessage(null);
-    }, 10000);
-  };
-
-  const padding = {
-    padding: 5,
-  };
-
-  return (
-    <Page>
-      <Navigation>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="/users">
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link style={padding} to="/login">
-            login
-          </Link>
-        )}
-      </Navigation>
-
-      <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />
-        <Route
-          path="/users"
-          element={user ? <Users /> : <Navigate replace to="/login" />}
-        />
-        <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-      <Footer>
-        <em>Note app, Department of Computer Science 2022</em>
-      </Footer>
-    </Page>
-  );
-};
+    );
+  }
+}
 
 export default App;
